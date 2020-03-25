ba3eeff8f8be05e370f48f7747af235de79f605c
Josef Fürlinger <josef.fuerlinger@gmail.com>
2020-03-25 20:45:42 +0100

    add serta-uplevel and serta-downlevel

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 // inspiration: https://github.com/abalabahaha/eris/blob/master/examples/basicCommands.js
 
-const StorageUtils = require('./storage');
+const StorageUtils = require('./utils/storage-utils.js');
+const LevelUtils = require('./utils/level-utils.js');
 
 // Run dotenv
 require('dotenv').config();
@@ -9,11 +10,15 @@ const config = require("./config.json");
 const createLogger = require('logging').default;
 const logger = createLogger('bot');
 
+logger.info(`AZURE_BLOBSTORAGE_CONNECTIONSTRING=${process.env.AZURE_BLOBSTORAGE_CONNECTIONSTRING}`);
+logger.info(`BOT_PREFIX=${process.env.BOT_PREFIX}`);
+logger.info(`BOT_INSTANCE_NAME=${process.env.BOT_INSTANCE_NAME}`);
+
 const Eris = require("eris");
 
 // Replace BOT_TOKEN with your bot account's token
 const bot = new Eris.CommandClient(process.env.DISCORD_TOKEN, {}, {
-    description: "------ Serta is everywhere ------ [" + process.env.BOT_INSTANCE_NAME + "]" ,
+    description: "------ Serta is everywhere ------ [" + process.env.BOT_INSTANCE_NAME + "]",
     owner: "J.Fürlinger / P.Bauer",
     prefix: process.env.BOT_PREFIX
 });
@@ -26,43 +31,98 @@ bot.on("ready", () => { // When the bot is ready
 
 bot.registerCommandAlias("halp", "help"); // Alias !halp to !help
 
+bot.registerCommand("serta-uplevel", async (msg, args) => {
 
-bot.registerCommand("removepoints", async (msg, args) => {
-
-    let localState = await StorageUtils.getState();
-    localStorage = localState
-        .map(entry => {
-            entry.points -= 10;
-            //return entry;
-        });
-
-    await StorageUtils.persistState(localState);
-
-    bot.createMessage(msg.channel.id, '10 points removed!');
-});
-
-bot.registerCommand("addpoints", async (msg, args) => {
+    logger.info(msg);
+    logger.info(args);
 
     let localState = await StorageUtils.getState();
     localStorage = localState
         .map(entry => {
-            if (entry.username === msg.author.username) {
-                entry.experience += 10;
+
+            if (!entry.level) {
+                entry.level = LevelUtils.getDefaultLevel();
             }
 
-            entry.color = 0xf5da42;
+            if (entry.username === msg.author.username) {
+                entry.level = LevelUtils.getNextLevel(entry.level.id);
+
+                bot.createMessage(msg.channel.id, `*${msg.author.username}* is now on level **${entry.level.name}**`);
+            }
+
         });
 
-    await StorageUtils.persistState(localState);
-
-    bot.createMessage(msg.channel.id, '10 Points added to your user!');
+     await StorageUtils.persistState(localState);
 });
 
-bot.registerCommand("statistics", async (msg, args) => {
+bot.registerCommandAlias("su",  "serta-uplevel"); 
+
+bot.registerCommand("serta-downlevel", async (msg, args) => {
+
+    logger.info(msg);
+    logger.info(args);
+
+    let localState = await StorageUtils.getState();
+    localStorage = localState
+        .map(entry => {
+
+            if (!entry.level) {
+                entry.level = LevelUtils.getDefaultLevel();
+            }
+
+            if (entry.username === msg.author.username) {
+                entry.level = LevelUtils.getPrevLevel(entry.level.id);
+
+                bot.createMessage(msg.channel.id, `*${msg.author.username}* is now on level **${entry.level.name}**`);
+            }
+
+        });
+
+     await StorageUtils.persistState(localState);
+});
+
+bot.registerCommandAlias("sd",  "serta-downlevel"); 
+
+
+// bot.registerCommand("removepoints", async (msg, args) => {
+
+//     let localState = await StorageUtils.getState();
+//     localStorage = localState
+//         .map(entry => {
+//             entry.points -= 10;
+//             //return entry;
+//         });
+
+//     await StorageUtils.persistState(localState);
+
+//     bot.createMessage(msg.channel.id, '10 points removed!');
+// });
+
+// bot.registerCommand("addpoints", async (msg, args) => {
+
+//     let localState = await StorageUtils.getState();
+//     localStorage = localState
+//         .map(entry => {
+//             if (entry.username === msg.author.username) {
+//                 entry.experience += 10;
+//             }
+
+//             entry.color = 0xf5da42;
+//         });
+
+//     await StorageUtils.persistState(localState);
+
+//     bot.createMessage(msg.channel.id, '10 Points added to your user!');
+// });
+
+bot.registerCommand("serta-statistics", async (msg, args) => {
 
     let localStorage = await StorageUtils.getState();
 
     localStorage.map(entry => {
+
+        logger.info(`levelDescription=${entry.level.description}`);
+
         bot.createMessage(msg.channel.id, {
             embed: {
                 title: "Status", // Title of the embed
@@ -73,6 +133,16 @@ bot.registerCommand("statistics", async (msg, args) => {
                 },
                 color: entry.color ? entry.color : 0x008000, // Color, either in hex (show), or a base-10 integer
                 fields: [ // Array of field objects
+                    {
+                        name: "Level",
+                        value: entry.level ? entry.level.name : LevelUtils.getDefaultLevel().name,
+                        inline: true
+                    },
+                    {
+                        name: "Level Id",
+                        value: entry.level ? entry.level.id : LevelUtils.getDefaultLevel().id,
+                        inline: true
+                    },
                     {
                         name: "Health",
                         value: entry.health,
@@ -91,11 +161,8 @@ bot.registerCommand("statistics", async (msg, args) => {
             }
         });
     });
-
-
-
 });
 
-bot.registerCommandAlias("stats", "statistics"); // Alias !stats to !statistics
+bot.registerCommandAlias("ss", "serta-statistics"); 
 
 bot.connect();
---
+++ utils/level-utils.js
@@ -0,0 +1,40 @@
+const createLogger = require('logging').default;
+const logger = createLogger('level-utils');
+
+const levels = [
+    {
+        id: 0,
+        abbreviation: 'S',
+        name: 'Loops'
+    },
+    {
+        id: 1,
+        abbreviation: 'A',
+        name: 'Arrays'
+    },
+    {
+        id: 2,
+        abbreviation: 'M',
+        name: 'Methods'
+    },
+    {
+        id: 3,
+        abbreviation: 'C',
+        name: 'Classes'
+    },
+    {
+        id: 4,
+        abbreviation: 'F',
+        name: 'Files'
+    },
+    {
+        id: 5,
+        abbreviation: 'P',
+        name: 'Greenfoot Serta figher'
+    },
+];
+
+module.exports.getNextLevel = (levelIdx) => levels[Math.min(levelIdx + 1, levels.length-1)];
+module.exports.getPrevLevel = (levelIdx) => levels[Math.max(0, levelIdx - 1)];
+
+module.exports.getDefaultLevel = () => levels[0];
