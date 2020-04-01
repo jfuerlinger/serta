ff80d3da9682a1aa56a7f630c7ff649e7464600a
Peter Bauer <p.bauer@htl-leonding.ac.at>
2020-04-01 17:37:49 +0200

    Renamed test

--- src/services/serta-user-service.test.ts
+++ src/services/serta-user-service.test.ts
@@ -4,13 +4,13 @@ import { TableStorageUserDao } from "../dao/table-storage/table-storage-user-dao
 
 describe("SertaUserService", () => {
   const bot = new SertaBot()
+  const guildId = '692047042154987527' // TODO: not to be hidden here
 
   beforeAll(async () => {
     await bot.run()
   })
 
-  test('shall return all users from Discord server when getUsers is called', async () => {
-    const guildId = '692047042154987527' // TODO: not to be hidden here
+  test('getUsers shall return a number of three test users', async () => {
     const userDao = new TableStorageUserDao(guildId);
     const userService = new SertaUserService(bot._bot, userDao);
     let users = await userService.GetUsers(guildId);
