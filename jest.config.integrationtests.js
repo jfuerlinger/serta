7b0b0ef7f85aca5737bbcf8b0d903f79c8bf2ee4
Peter Bauer <p.bauer@htl-leonding.ac.at>
2020-04-06 10:24:08 +0200

    Added matchers to jest.config.*.js files to avoid testing of js files

--- jest.config.integrationtests.js
+++ jest.config.integrationtests.js
@@ -1,5 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    rootDir: 'tests/integration-tests'
+    rootDir: 'tests/integration-tests',
+    testMatch: [ "**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).ts?(x)" ]
 };
