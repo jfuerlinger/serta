0b98cee9186291b6d08ae16245c2d66c8e26a2eb
Peter Bauer <p.bauer@htl-leonding.ac.at>
2020-05-04 22:14:12 +0200

    Replaced switch case by array

--- src/tests/unit-tests/test-doubles/fake-user-service.ts
+++ src/tests/unit-tests/test-doubles/fake-user-service.ts
@@ -3,6 +3,18 @@ import {ISertaUser} from "../../../model/i-serta-user";
 import {FakeSertaUser} from "./fake-serta-user";
 
 export class FakeUserService implements IUserService {
+    private fakeUsers: FakeSertaUser[]
+
+    constructor() {
+        const now = Date.now()
+        const lastInfection = now - (3 * 60 * 60 + 22 * 60 + 12) * 1000
+        this.fakeUsers = [
+            new FakeSertaUser("some.discord.id", "p.bauer", "http://avatarUrl/pb.png", 1, 35, 15, new Date(lastInfection)),
+            new FakeSertaUser("another.discord.id", "jfuerlinger", "http://avatarUrl/jf.png", 3, 187, 45),
+            new FakeSertaUser("bot.discord.id", "Serta", "", 0, 0, 0, undefined, true)
+        ]
+    }
+
     update(user: ISertaUser): Promise<void> {
         return new Promise<void>(() => {
         });
@@ -30,22 +42,11 @@ export class FakeUserService implements IUserService {
 
     getByDiscordUserName(discordUserName: string): Promise<ISertaUser> {
         return new Promise<ISertaUser>(async resolve => {
-            switch (discordUserName) {
-                case "p.bauer":
-                    const now = Date.now()
-                    const lastInfection = now - (3 * 60 * 60 + 22 * 60 + 12) * 1000
-                    resolve(new FakeSertaUser("some.discord.id", "p.bauer", "http://avatarUrl/pb.png", 1, 35, 15, new Date(lastInfection)))
-                    break
-
-                case "jfuerlinger":
-                    resolve(new FakeSertaUser("another.discord.id", "jfuerlinger", "http://avatarUrl/jf.png", 3, 187, 45))
-                    break
-                case "Serta":
-                    resolve(new FakeSertaUser("bot.discord.id", "Serta", "", 0, 0, 0, undefined, true))
-                    break
-
-                default:
-                    resolve(undefined)
+            const foundUser = this.fakeUsers.filter(u => u.discordUserName === discordUserName)
+            if (foundUser.length > 0) {
+                resolve(foundUser[0])
+            } else {
+                resolve(undefined)
             }
         })
     }
